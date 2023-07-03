f20574f1c4d6be2f62fad473c701006bc6e1bdeb
ViperStatus <136202018+ViperStatus@users.noreply.github.com>
2023-07-02 19:46:41 -0500

    main.py
    Accidentally overwritten with 6.17 program.  I pasted it back from my history on Github.

--- Homework2/main.py
+++ Homework2/main.py
@@ -1,25 +1,16 @@
-#Sokheng Ka 1968133
-
-#Enter current password
-pw = input()
-
-password = ''
+# Sokheng Ka 1968133
+import datetime
 
 
-
-for character in pw:
-
-    if character == 'i':
-        pw = pw.replace('i', '!')
-    elif character == 'a':
-        pw = pw.replace('a', '@')
-    elif character == 'm':
-        pw = pw.replace('m', 'M')
-    elif character == 'B':
-        pw = pw.replace('B', '8')
-    elif character == 'o':
-        pw = pw.replace('o', '.')
-    else:
-        password += character
-
-print(pw+'q*s')
+def find(extract_date):
+    mthtonum = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
+                'September': 9, 'October': 10, 'November': 11, 'December': 12}
+@@ -19,4 +18,7 @@ def find(extract_date):
+    with open('inputDates.txt') as f:
+        for x in f.readlines():
+            if x.strip() != '-1':
+    print(fid(x.strip()))
+    res = find(x.strip()))
+    if res != '':
+        with open('parsedDAtes.txt') as w:
+            w.write(res + '/n')

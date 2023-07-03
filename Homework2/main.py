797621cb73db5db0dde61a4841ce6e07cac80aaf
ViperStatus <136202018+ViperStatus@users.noreply.github.com>
2023-07-02 19:05:26 -0500

    Update main.py
    Edit on B

--- Homework2/main.py
+++ Homework2/main.py
@@ -5,7 +5,7 @@ import datetime
 def find(extract_date):
     mthtonum = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8,
                 'September': 9, 'October': 10, 'November': 11, 'December': 12}
-
+try:
     yy = extract_date.split(',')[-1].strip()
     mm = extract_date.split(',')[0].split()[0]
     dd = extract_date.split(',')[0].split()[-1]
@@ -13,8 +13,8 @@ def find(extract_date):
     int(yy)
     int(dd)
     return str(mn) + '/' + dd + '/' + yy
-    except:
-        return''
+except:
+    return ''
 
     with open('inputDates.txt') as f:
         for x in f.readlines():
